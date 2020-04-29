498524ff8c48164e8858bb9bec33d471695166d7
Gagan Bhat <gaganbhat6@gmail.com>
2020-04-29 10:14:12 -0500

    Basic login screen

--- src/App.js
+++ src/App.js
@@ -4,21 +4,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Login demo</h3>
+      <form>
+        <label>
+          Username:
+          <input type="text" name="username"/>
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password"/>
+        </label>
+        <input type="submit" name="submit"/>
+      </form>
+       
     </div>
   );
 }
