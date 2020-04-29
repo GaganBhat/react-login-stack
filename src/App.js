250d93e2af797c2a8576c684601a0309cbfcbe3e
Gagan Bhat <gaganbhat6@gmail.com>
2020-04-29 18:35:19 -0500

    Working aws amplify react login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {withAuthenticator} from 'aws-amplify-react'
 
 function App() {
   return (
     <div>
-      <h3>Login demo</h3>
-      <form>
-        <label>
-          Username:
-          <input type="text" name="username"/>
-        </label>
-        <label>
-          Password:
-          <input type="password" name="password"/>
-        </label>
-        <input type="submit" name="submit"/>
-      </form>
-       
+      Authenticated, landing page.
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, {
+  includeGreetings: true
+});
---
+++ src/amplifyconfig.js
@@ -0,0 +1,12 @@
+export default {
+    apiGateway: {
+        REGION: "",
+        URL: ""
+    },
+    cognito: {
+        REGION: "",
+        USER_POOL_ID: "",
+        APP_CLIENT_ID: "",
+        IDENTITIY_POOL_ID: ""
+    }
+}
